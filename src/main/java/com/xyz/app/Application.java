49ef3bd892aff19026e8a2b361b243144ffae44a
gxgwyj <498868779@qq.com>
2018-01-18 17:31:17 +0800

    xyz:数据源添加

--- src/main/java/com/xyz/app/Application.java
+++ src/main/java/com/xyz/app/Application.java
@@ -2,14 +2,13 @@ package com.xyz.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
 /**
  * 启动应用程序主类
  * Created by Lenovo on 2018/1/6.
  */
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication
 @ServletComponentScan
 public class Application {
     public static void main(String[] args) {
